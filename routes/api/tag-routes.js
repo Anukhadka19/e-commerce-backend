e9fa89b134b176f86d6d013f44f736b0472b2a07
Anu Khadka <anuragi.khadka@gmail.com>
2023-03-08 00:21:57 -0500

    added through ProductTag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,7 +6,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag, as: 'product_tag' }],
 });
 res.status(200).json(tagData);
   } catch (err) {
@@ -17,7 +17,7 @@ res.status(200).json(tagData);
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag, as: 'product_tag' }],
     });
     if (!tagData) {
       res.status(404).json({ message: 'Tag not found' });
